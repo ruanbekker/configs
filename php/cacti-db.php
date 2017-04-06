1be30cd15cec783f56a1b3e6887284c7cea88e74
Ruan <ruan.ru.bekker@gmail.com>
2017-04-06 11:00:41 +0000

    Add new file

---
+++ php/cacti-db.php
@@ -0,0 +1,9 @@
+<?php
+$database_type = "mysql";
+$database_default = "cacti";
+$database_hostname = "localhost";
+$database_username = "cacti";
+$database_password = "zYn95ph43zYtq";
+$database_port = "3306";
+$database_ssl = false;
+?>
